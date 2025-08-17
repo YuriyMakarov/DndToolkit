b0fa5495d41f3fd8f7cf8d9a732d3644228f5c48
name <mail>
2025-08-17 03:16:01 +0300

    Init commit

---
+++ DndToolkit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DndToolkit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
